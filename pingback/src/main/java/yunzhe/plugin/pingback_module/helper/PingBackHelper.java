e45de483589fc155f5e3a555f441430b70bbf0e0
刘浩 <liuhao01@finupgroup.com>
2019-09-27 17:43:45 +0800

    版本号升级,合并功能

--- pingback/src/main/java/yunzhe/plugin/pingback_module/helper/PingBackHelper.java
+++ pingback/src/main/java/yunzhe/plugin/pingback_module/helper/PingBackHelper.java
@@ -1,5 +1,12 @@
 package yunzhe.plugin.pingback_module.helper;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
 import org.json.JSONObject;
 
 import java.lang.reflect.Field;
@@ -116,5 +123,49 @@ public class PingBackHelper {
             saveEventKeysHashMap.remove(eventKey);
             saveEventKeysHashMap.put(eventKey, new YZHashMap<String, Object>());
         }
+    }
+
+    /**
+     * 根据context获取不同activity的label名称
+     *
+     * @param context
+     * @return 对应的activity在AndroidManifest.xml中设置的label
+     */
+    public static String getActivityLabelName(Context context, Class className) {
+        int labelRes = 0;
+        try {
+            PackageInfo packageInfo = context.getPackageManager().
+                    getPackageInfo(context.getPackageName(), PackageManager.GET_ACTIVITIES);
+
+            for (int i = 0; i < packageInfo.activities.length; i++) {
+                ActivityInfo ai = packageInfo.activities[i];
+                if (ai.name.equals(className.getName())) {
+                    labelRes = ai.labelRes;
+                    break;
+                }
+            }
+            if (labelRes != 0)
+                return context.getString(labelRes);
+            return null;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 根据context获取不同activity的label名称
+     */
+    @SuppressLint("WrongConstant")
+    public static String getActivityLabelName(Context context) {
+        try {
+            ActivityInfo activityInfo = context.getPackageManager().getActivityInfo(((Activity) context).getComponentName(), PackageManager.GET_ACTIVITIES);
+            int labelRes = activityInfo.labelRes;
+            String activityName = activityInfo.name;
+            return context.getString(labelRes);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
